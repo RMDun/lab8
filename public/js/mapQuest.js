3f0ec2063d483a83fb1d57d9f50ff3ab1cf0b3fb
Rachel Duncan <rmduncan@ucsd.edu>
2019-02-27 20:39:16 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'jGyqCflmXGOJc9FhSt0F1A13PgY1zfq3';
+
+	var map = L.mapquest.map('map', {
+		center: [32.879101, -117.235934],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker([32.879101, -117.235934]).addTo(map);
 }
